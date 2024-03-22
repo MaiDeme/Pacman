af9ed32617f93b466ccbc394d5853488071b359a
Camille De Amorim <camille.deamorim17@gmail.com>
2024-03-22 13:10:26 +0100

    TestBoard

--- src/fr/upsaclay/bibs/pacman/model/board/TestBoard.java
+++ src/fr/upsaclay/bibs/pacman/model/board/TestBoard.java
@@ -2,20 +2,26 @@ package fr.upsaclay.bibs.pacman.model.board;
 
 import fr.upsaclay.bibs.pacman.GameType;
 import fr.upsaclay.bibs.pacman.PacManException;
+import fr.upsaclay.bibs.pacman.model.maze.Grid;
+import fr.upsaclay.bibs.pacman.model.maze.Maze;
 
-public class TestBoard implements Board {
+import java.io.FileNotFoundException;
+
+public class TestBoard extends AbstractBoard {
 
 
-
-    @Override
-    public GameType getGameType() {
-        return GameType.TEST;
+    public TestBoard() {
+        super(GameType.TEST);
     }
+
 
     @Override
     public void initialize() throws PacManException {
-        this.getMaze().
-
+        try {
+            this.maze = Maze.loadFromFile("test.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
