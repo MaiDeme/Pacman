9024b5d1bee12ce10f8f65afd3dd7193988502bf
antoine.loth <antoine.loth@universite-paris-saclay.fr>
2024-03-22 12:36:05 +0100

    1.6.2
    Finalisation de la classe ClassicBoard

--- src/fr/upsaclay/bibs/pacman/model/board/ClassicBoard.java
+++ src/fr/upsaclay/bibs/pacman/model/board/ClassicBoard.java
@@ -1,6 +1,9 @@
 package fr.upsaclay.bibs.pacman.model.board;
 import fr.upsaclay.bibs.pacman.GameType;
 import fr.upsaclay.bibs.pacman.PacManException;
+import fr.upsaclay.bibs.pacman.model.maze.Maze;
+
+import java.io.FileNotFoundException;
 
 public class ClassicBoard extends AbstractBoard {
     public ClassicBoard() {
@@ -14,7 +17,12 @@ public class ClassicBoard extends AbstractBoard {
 
     @Override
     public void initialize() throws PacManException {
-
+        try {
+            this.maze = Maze.loadFromFile("maze.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
 }
