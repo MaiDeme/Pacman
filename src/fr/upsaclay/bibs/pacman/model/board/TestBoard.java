cf0353ac7ee019948a6fce5f99f3d7cf0b8c6049
Camille De Amorim <camille.deamorim17@gmail.com>
2024-03-22 12:52:11 +0100

    protected pour abstractboard

--- src/fr/upsaclay/bibs/pacman/model/board/TestBoard.java
+++ src/fr/upsaclay/bibs/pacman/model/board/TestBoard.java
@@ -1,4 +1,19 @@
 package fr.upsaclay.bibs.pacman.model.board;
 
+import fr.upsaclay.bibs.pacman.GameType;
+import fr.upsaclay.bibs.pacman.PacManException;
+
 public class TestBoard implements Board {
+
+
+    @Override
+    public GameType getGameType() {
+        return GameType.TEST;
+    }
+
+    @Override
+    public void initialize() throws PacManException {
+
+
+    }
 }
