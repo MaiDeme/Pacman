1fdf5e9f63f73e0fb30a8f7149efcafb977a151a
antoine.loth <antoine.loth@universite-paris-saclay.fr>
2024-04-02 09:21:05 +0200

    Sound Beginning
    Ajout de la classe SoundManager.java, SoundName.java et implémentation du son lorsqu'on lance le jeu

---
+++ src/fr/upsaclay/bibs/pacman/audio/SoundManager.java
@@ -0,0 +1,54 @@
+package fr.upsaclay.bibs.pacman.audio;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SoundManager {
+    private Map<String, Clip> soundClips;
+
+    public SoundManager() {
+        soundClips = new HashMap<>();
+        loadSounds();
+    }
+
+    private void loadSound(String name, String filePath) {
+        try {
+            File soundFile = new File(filePath);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            soundClips.put(name, clip);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadSounds() {
+        loadSound("PACMAN_CHOMP", "resources/sounds/pacman_chomp.wav");
+        loadSound("PACMAN_BEGINNING", "resources/sounds/pacman_beginning.wav");
+        // Load other sounds as needed
+    }
+
+    public void play(String soundName) {
+        Clip clip = soundClips.get(soundName);
+        if (clip != null) {
+            if (clip.isRunning()) {
+                clip.stop();
+            }
+            clip.setFramePosition(0);
+            clip.start();
+        }
+    }
+
+    public void stop(String soundName) {
+        Clip clip = soundClips.get(soundName);
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+        }
+    }
+
+    // Add other methods for pause, resume, etc., as needed
+}
